97febea6a2676003a18b7b974e8e45f153ee09b4
Corvin Köhne <c.koehne@beckhoff.com>
2021-06-15 10:41:09 +0200

    bhyve/ROM: add func for allocating a ROM
    Will be used by passthru emulation to allocate a ROM BAR if a romfile
    is specified.
    
    Signed-off-by: Corvin Köhne <c.koehne@beckhoff.com>
    Reviewed-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- usr.sbin/bhyve/pci_emul.h
+++ usr.sbin/bhyve/pci_emul.h
@@ -166,6 +166,7 @@ struct pci_devinst {
 	u_char	  pi_cfgdata[PCI_REGMAX + 1];
 	/* ROM is handled like a BAR */
 	struct pcibar pi_bar[PCI_BARMAX_WITH_ROM + 1];
+	uint64_t pi_romoffset;
 };
 
 struct msicap {
@@ -229,6 +230,7 @@ int	init_pci(struct vmctx *ctx);
 void	pci_callback(void);
 int	pci_emul_alloc_bar(struct pci_devinst *pdi, int idx,
 	    enum pcibar_type type, uint64_t size);
+int pci_emul_alloc_rom(struct pci_devinst *const pdi, const uint64_t size, uint64_t *const addr);
 int	pci_emul_add_msicap(struct pci_devinst *pi, int msgnum);
 int	pci_emul_add_pciecap(struct pci_devinst *pi, int pcie_device_type);
 void	pci_emul_capwrite(struct pci_devinst *pi, int offset, int bytes,
