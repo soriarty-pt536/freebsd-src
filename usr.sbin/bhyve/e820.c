407b8defb5d142624205cf26782f8687e5d49265
Corvin Köhne <c.koehne@beckhoff.com>
2022-03-17 13:11:05 +0100

    bhyve/e820: add basic implementation
    Bhyve will use the E820 table to reserve some memory ranges especially
    graphics memory for GPU passthrough.
    
    Signed-off-by: Corvin Köhne <c.koehne@beckhoff.com>
    Reviewed-by: Patrick Bruenn <p.bruenn@beckhoff.com>

---
+++ usr.sbin/bhyve/e820.c
@@ -0,0 +1,231 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2021 Beckhoff Automation GmbH & Co. KG
+ * Author: Corvin Köhne <c.koehne@beckhoff.com>
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/types.h>
+#include <sys/queue.h>
+
+#include <machine/vmm.h>
+
+#include <err.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "e820.h"
+#include "qemu_fwcfg.h"
+
+#define E820_FWCFG_FILE_NAME "etc/e820"
+
+#define KB (1024UL)
+#define MB (1024 * KB)
+#define GB (1024 * MB)
+
+struct e820_element {
+	TAILQ_ENTRY(e820_element) chain;
+	uint64_t base;
+	uint64_t end;
+	enum e820_memory_type type;
+};
+TAILQ_HEAD(e820_table, e820_element) e820_table = TAILQ_HEAD_INITIALIZER(
+    e820_table);
+
+struct qemu_fwcfg_item *
+e820_get_fwcfg_item()
+{
+	uint64_t count = 0;
+	struct e820_element *element;
+	TAILQ_FOREACH (element, &e820_table, chain) {
+		++count;
+	}
+	if (count == 0) {
+		warnx("%s: E820 table empty", __func__);
+		return (NULL);
+	}
+
+	struct qemu_fwcfg_item *const fwcfg_item = malloc(
+	    sizeof(struct qemu_fwcfg_item));
+	if (fwcfg_item == NULL) {
+		return (NULL);
+	}
+	fwcfg_item->size = count * sizeof(struct e820_entry);
+	fwcfg_item->data = malloc(fwcfg_item->size);
+	if (fwcfg_item->data == NULL) {
+		free(fwcfg_item);
+		return (NULL);
+	}
+	uint64_t i = 0;
+	struct e820_entry *entries = (struct e820_entry *)fwcfg_item->data;
+	TAILQ_FOREACH (element, &e820_table, chain) {
+		struct e820_entry *entry = &entries[i];
+		entry->base = element->base;
+		entry->length = element->end - element->base;
+		entry->type = element->type;
+		++i;
+	}
+
+	return fwcfg_item;
+}
+
+int
+e820_add_entry(const uint64_t base, const uint64_t end,
+    const enum e820_memory_type type)
+{
+	if (end < base) {
+		return (-1);
+	}
+
+	struct e820_element *const new_element = malloc(
+	    sizeof(struct e820_element));
+	if (new_element == NULL) {
+		return (-ENOMEM);
+	}
+
+	new_element->base = base;
+	new_element->end = end;
+	new_element->type = type;
+
+	/*
+	 * E820 table should be always sorted in ascending order. Therefore,
+	 * search for an element which end is larger than the base parameter.
+	 */
+	struct e820_element *element;
+	TAILQ_FOREACH (element, &e820_table, chain) {
+		if (element->end > base) {
+			break;
+		}
+	}
+
+	/*
+	 * System memory requires special handling.
+	 */
+	if (type == E820_TYPE_MEMORY) {
+		/*
+		 * base is larger than of any existing element. Add new system
+		 * memory at the end of the table.
+		 */
+		if (element == NULL) {
+			TAILQ_INSERT_TAIL(&e820_table, new_element, chain);
+			return (0);
+		}
+
+		/*
+		 * System memory shouldn't overlap with any existing element.
+		 */
+		if (end > element->base) {
+			return (-1);
+		}
+		TAILQ_INSERT_BEFORE(element, new_element, chain);
+		return (0);
+	}
+
+	if (element == NULL) {
+		/* No suitable element found */
+		return (-1);
+	}
+
+	/*
+	 * Non system memory should be allocated inside system memory.
+	 */
+	if (element->type != E820_TYPE_MEMORY) {
+		return (-1);
+	}
+	/*
+	 * New element should fit into existing system memory element.
+	 */
+	if (base < element->base || end > element->end) {
+		return (-1);
+	}
+
+	if (base == element->base) {
+		/*
+		 * New element at system memory base boundary. Add new
+		 * element before current and adjust the base of the old
+		 * element.
+		 *
+		 * Old table:
+		 * 	[ 0x1000, 0x4000] RAM		<-- element
+		 * New table:
+		 * 	[ 0x1000, 0x2000] Reserved
+		 * 	[ 0x2000, 0x4000] RAM		<-- element
+		 */
+		TAILQ_INSERT_BEFORE(element, new_element, chain);
+		element->base = end;
+	} else if (end == element->end) {
+		/*
+		 * New element at system memory end boundary. Add new
+		 * element after current and adjust the end of the
+		 * current element.
+		 *
+		 * Old table:
+		 * 	[ 0x1000, 0x4000] RAM		<-- element
+		 * New table:
+		 * 	[ 0x1000, 0x3000] RAM		<-- element
+		 * 	[ 0x3000, 0x4000] Reserved
+		 */
+		TAILQ_INSERT_AFTER(&e820_table, element, new_element, chain);
+		element->end = base;
+	} else {
+		/*
+		 * New element inside system memory entry. Split it by
+		 * adding a system memory element and the new element
+		 * before current.
+		 *
+		 * Old table:
+		 * 	[ 0x1000, 0x4000] RAM		<-- element
+		 * New table:
+		 * 	[ 0x1000, 0x2000] RAM
+		 * 	[ 0x2000, 0x3000] Reserved
+		 * 	[ 0x3000, 0x4000] RAM		<-- element
+		 */
+		struct e820_element *ram_element = malloc(
+		    sizeof(struct e820_element));
+		if (ram_element == NULL) {
+			return (-ENOMEM);
+		}
+		ram_element->base = element->base;
+		ram_element->end = base;
+		ram_element->type = E820_TYPE_MEMORY;
+		TAILQ_INSERT_BEFORE(element, ram_element, chain);
+		TAILQ_INSERT_BEFORE(element, new_element, chain);
+		element->base = end;
+	}
+
+	return (0);
+}
+
+int
+e820_init(struct vmctx *const ctx)
+{
+	int error;
+
+	TAILQ_INIT(&e820_table);
+
+	/* add memory below 4 GB to E820 table */
+	const uint64_t lowmem_length = vm_get_lowmem_size(ctx);
+	error = e820_add_entry(0, lowmem_length, E820_TYPE_MEMORY);
+	if (error) {
+		warnx("%s: Could not add lowmem", __func__);
+		return (error);
+	}
+
+	/* add memory above 4 GB to E820 table */
+	const uint64_t highmem_length = vm_get_highmem_size(ctx);
+	if (highmem_length != 0) {
+		error = e820_add_entry(4 * GB, 4 * GB + highmem_length,
+		    E820_TYPE_MEMORY);
+		if (error) {
+			warnx("%s: Could not add highmem", __func__);
+			return (error);
+		}
+	}
+
+	return (0);
+}
